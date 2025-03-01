a3987386377187c0ff13d09ec4268af89bc6926f
nimigeanconstantinion <constantin.nimigean@gmail.com>
2025-03-01 17:29:35 +0200

    commit 01032025 17.29

--- applyfiles.bat
+++ applyfiles.bat
@@ -1,11 +1,13 @@
 kubectl apply -f .\pv-pvc.yaml
-kubectl apply -f .\pv-pvc-tabbitmq.yaml
+kubectl apply -f .\pv-pvc-rabbitmq.yaml
 kubectl apply -f .\pv-pvc-elasticsearch.yaml
 kubectl apply -f .\pv-pvc-kibana.yaml
+
 kubectl apply -f .\secret.yaml
 kubectl apply -f .\configmap.yaml
 kubectl apply -f .\mysql-statefulset.yaml
 kubectl apply -f .\elastic-statefulset.yaml
+
 
 kubectl apply -f .\deployment-query.yaml
 kubectl apply -f .\deployment-command.yaml
@@ -15,9 +17,8 @@ kubectl apply -f .\deployment-client.yaml
 kubectl apply -f .\deployment-rabbitMQ.yaml
 kubectl apply -f .\deployment-logstash.yaml
 kubectl apply -f .\deployment-kibana.yaml
-
-kubectl apply -f .\setup-job.yaml
-
 kubectl apply -f .\ingress-v2.yaml
+
+
 
 
